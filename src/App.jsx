9bc9ceda715a8d2fa095d83eca680e6605d7418b
leonardopalluzzi <lp.palluzzi@gmail.com>
2025-08-26 17:56:09 +0200

    remove useless imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import AdminLayout from "./Layouts/AdminLayout"
-import EmployeeLayout from "./Layouts/EmployeeLayout"
-import CustomerLayout from "./Layouts/CustomerLayout"
-import AdminDashboard from "./Pages/Admin/AdminDashboard"
-import EmployeeDashboard from "./Pages/Employee/EmployeeDashboard"
-import CustomerDashboard from "./Pages/Customer/CustomerDashboard"
 import Login from "./Pages/Common/Login"
 import Register from "./Pages/Common/Register"
 import { AuthProvider } from "./Contexts/AuthContext"
-import { Navigate } from "react-router-dom";
-import RouteProtectionLayout from "./Layouts/RouteProtectionLayout"
 import Logout from "./Pages/Common/Logout"
 import RenderRoutes from "./Components/RoutesProtection/RenderRoutes"
 
