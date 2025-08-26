d4cc60386b9be09fb40e72e8e8c1581f1bce7e35
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-08-27 01:53:51 +0200

    add fetch data to admin dashboard

--- src/Pages/Admin/AdminDashboard.jsx
+++ src/Pages/Admin/AdminDashboard.jsx
@@ -1,8 +1,68 @@
+import { useEffect, useState } from "react"
+import LoaderUi from "../../Components/dumb/Loader.ui"
+import { useMessageContext } from "../../Contexts/MessageContext"
+import { useAuthContext } from "../../Contexts/AuthContext"
+
+
 export default function AdminDashboard() {
 
-    return (
-        <>
-            <h1>admin dashboard</h1>
-        </>
-    )
+    const { currentUser } = useAuthContext()
+    const { throwMessage } = useMessageContext()
+
+    const [stats, setStats] = useState({
+        state: 'loading'
+    })
+
+    useEffect(() => {
+        fetch(`${import.meta.env.VITE_BACK_URL}/api/v1/stats/company`, {
+            methodt: 'GET',
+            headers: {
+                'Authorization': `Bearer ${currentUser.token}`
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+
+                setStats({
+                    state: 'success',
+                    result: data
+                })
+                if (data.error) {
+                    throwMessage('error', [JSON.stringify(data)])
+                }
+            })
+            .catch(err => {
+                setStats({
+                    state: 'error',
+                    message: err.message
+                })
+                throwMessage('error', [err.message])
+            })
+
+    }, [])
+
+    switch (stats.state) {
+        case 'loading':
+            return (
+                <>
+                    <LoaderUi />
+                </>
+            )
+        case 'error':
+            return (
+                <div className="text-danger">
+                    {stats.message}
+                </div>
+            )
+        case 'success':
+            return (
+                <>
+                    <div className="container py-5">
+                        <h1>admin dashboard</h1>
+
+                    </div>
+                </>
+            )
+    }
 }
